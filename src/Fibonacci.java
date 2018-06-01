9e30699e533a1e3eebd29f6c33eb7de5a4a57a55
Lynette <lyna8907@gmail.com>
2018-06-01 15:23:18 -0500

    optimized palindrome and fibonacci

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,16 +1,14 @@
 public class Fibonacci {
 
     public static int fibNum(int n) {
-        if(n == 1) {
-            return 1;
-        } else if(n == 2) {
+        if(n == 1 || n == 2) {
             return 1;
         }
         return fibNum(n-1) + fibNum(n-2);
     }
 
     public static void main(String[] args) {
-        System.out.println(fibNum(7));
+        System.out.println(fibNum(8));
     }
 
 }
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,16 +1,20 @@
 public class Palindrome {
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("Asdffdsa"));
+        System.out.println(isPalindrome("asdffdsa"));
     }
 
+//    private static boolean isPalindrome(String x) {
+//        char[] xArray = x.toLowerCase().toCharArray();
+//        for(int i = 0; i < xArray.length/2; i++) {
+//            if(xArray[i] != xArray[xArray.length-i-1]) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
     private static boolean isPalindrome(String x) {
-        char[] xArray = x.toLowerCase().toCharArray();
-        for(int i = 0; i < xArray.length/2; i++) {
-            if(xArray[i] != xArray[xArray.length-i-1]) {
-                return false;
-            }
-        }
-        return true;
+        return new StringBuilder(x).reverse().toString().equals(new StringBuilder(x).toString());
     }
 }
